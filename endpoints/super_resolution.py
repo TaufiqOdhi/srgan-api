f409454b419125e3dae14f4b7c03c39dd4877672
Taufiq Odhi <taufiq.odhi@gmail.com>
2023-10-23 20:06:04 +0700

    update api for pruned model

--- endpoints/super_resolution.py
+++ endpoints/super_resolution.py
@@ -58,128 +58,363 @@ async def no_prune(image: UploadFile = File(), filename: str = Form()):
     
 
 async def random_unstructured_30(image: UploadFile = File(), filename: str = Form()):
+    start_timestamp = datetime.datetime.now()
     input_filename = f'{filename}_random_unstructured_30_{datetime.datetime.now()}{Path(image.filename).suffix}'
-    input_file = os.path.expanduser(f'~/Projects/Thesis/input_files/{input_filename}')
-    with open(input_file, "wb+") as file_object:
-        file_object.write(image.file.read())
-        file_object.close()
+    minio_host = os.popen("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio-server").read()
+    ip_host = os.popen("ip addr | awk '/inet/ && /docker0/ {print $2}'").read().split('/')[0]
+
+    # Upload FIle to Minio bucket
+    minio_client.put_object(
+        bucket_name=f'{bucket_name}',
+        object_name=f'input_files/{input_filename}',
+        data=image.file,
+        length=-1,  # Unknown size, read till EOF,
+        part_size=5 << 20 # 5MB chunks
+    )
+
+    # enqueue to redis
+    redis_client.lpush('srgan', json.dumps(dict(
+        filename=input_filename,
+        image_name='srgan-ai-module:random_unstructured-30',
+        ai_type='srgan'
+    )))
+
+    # nanti disebelah sini akan dijalankan sebuah metode untuk menentukan proses yang datang ini akan
+    # dimasukkan pada worker mana ?
+    # 
+
+    # dequeue from redis
+    item = json.loads(redis_client.rpop('srgan').decode('utf-8'))
+    print(item)
+
     cmd = ["docker", "run", "--rm", "--runtime", 'nvidia',
            "-v", os.path.expanduser("~/Projects/Thesis/input_files:/app/input_files"),
            "-v", os.path.expanduser("~/Projects/Thesis/output_files:/app/output_files"),
-           "-e", f'FILENAME={input_filename}', "taufiqodhi/srgan-ai-module:random_unstructured-30"]
+           "-e", f'FILENAME={input_filename}', "-e", f"MINIO_HOST={minio_host}",
+           "-e", f'IP_HOST={ip_host}', "-e", f"START_TIMESTAMP={start_timestamp}",
+           "taufiqodhi/srgan-ai-module:random_unstructured-30"]
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     return dict(result=completed_process.stdout, error=completed_process.stderr)
 
 
 async def random_unstructured_50(image: UploadFile = File(), filename: str = Form()):
+    start_timestamp = datetime.datetime.now()
     input_filename = f'{filename}_random_unstructured_50_{datetime.datetime.now()}{Path(image.filename).suffix}'
-    input_file = os.path.expanduser(f'~/Projects/Thesis/input_files/{input_filename}')
-    with open(input_file, "wb+") as file_object:
-        file_object.write(image.file.read())
-        file_object.close()
+    minio_host = os.popen("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio-server").read()
+    ip_host = os.popen("ip addr | awk '/inet/ && /docker0/ {print $2}'").read().split('/')[0]
+
+    # Upload FIle to Minio bucket
+    minio_client.put_object(
+        bucket_name=f'{bucket_name}',
+        object_name=f'input_files/{input_filename}',
+        data=image.file,
+        length=-1,  # Unknown size, read till EOF,
+        part_size=5 << 20 # 5MB chunks
+    )
+
+    # enqueue to redis
+    redis_client.lpush('srgan', json.dumps(dict(
+        filename=input_filename,
+        image_name='srgan-ai-module:random_unstructured-50',
+        ai_type='srgan'
+    )))
+
+    # nanti disebelah sini akan dijalankan sebuah metode untuk menentukan proses yang datang ini akan
+    # dimasukkan pada worker mana ?
+    # 
+
+    # dequeue from redis
+    item = json.loads(redis_client.rpop('srgan').decode('utf-8'))
+    print(item)
+
     cmd = ["docker", "run", "--rm", "--runtime", 'nvidia',
            "-v", os.path.expanduser("~/Projects/Thesis/input_files:/app/input_files"),
            "-v", os.path.expanduser("~/Projects/Thesis/output_files:/app/output_files"),
-           "-e", f'FILENAME={input_filename}', "taufiqodhi/srgan-ai-module:random_unstructured-50"]
+           "-e", f'FILENAME={input_filename}', "-e", f"MINIO_HOST={minio_host}",
+           "-e", f'IP_HOST={ip_host}', "-e", f"START_TIMESTAMP={start_timestamp}",
+           "taufiqodhi/srgan-ai-module:random_unstructured-50"]
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     return dict(result=completed_process.stdout, error=completed_process.stderr)
 
 async def random_unstructured_70(image: UploadFile = File(), filename: str = Form()):
+    start_timestamp = datetime.datetime.now()
     input_filename = f'{filename}_random_unstructured_70_{datetime.datetime.now()}{Path(image.filename).suffix}'
-    input_file = os.path.expanduser(f'~/Projects/Thesis/input_files/{input_filename}')
-    with open(input_file, "wb+") as file_object:
-        file_object.write(image.file.read())
-        file_object.close()
+    minio_host = os.popen("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio-server").read()
+    ip_host = os.popen("ip addr | awk '/inet/ && /docker0/ {print $2}'").read().split('/')[0]
+
+    # Upload FIle to Minio bucket
+    minio_client.put_object(
+        bucket_name=f'{bucket_name}',
+        object_name=f'input_files/{input_filename}',
+        data=image.file,
+        length=-1,  # Unknown size, read till EOF,
+        part_size=5 << 20 # 5MB chunks
+    )
+
+    # enqueue to redis
+    redis_client.lpush('srgan', json.dumps(dict(
+        filename=input_filename,
+        image_name='srgan-ai-module:random_unstructured-70',
+        ai_type='srgan'
+    )))
+
+    # nanti disebelah sini akan dijalankan sebuah metode untuk menentukan proses yang datang ini akan
+    # dimasukkan pada worker mana ?
+    # 
+
+    # dequeue from redis
+    item = json.loads(redis_client.rpop('srgan').decode('utf-8'))
+    print(item)
+
     cmd = ["docker", "run", "--rm", "--runtime", 'nvidia',
            "-v", os.path.expanduser("~/Projects/Thesis/input_files:/app/input_files"),
            "-v", os.path.expanduser("~/Projects/Thesis/output_files:/app/output_files"),
-           "-e", f'FILENAME={input_filename}', "taufiqodhi/srgan-ai-module:random_unstructured-70"]
+           "-e", f'FILENAME={input_filename}', "-e", f"MINIO_HOST={minio_host}",
+           "-e", f'IP_HOST={ip_host}', "-e", f"START_TIMESTAMP={start_timestamp}",
+           "taufiqodhi/srgan-ai-module:random_unstructured-70"]
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     return dict(result=completed_process.stdout, error=completed_process.stderr)
 
 async def l1_norm_30(image: UploadFile = File(), filename: str = Form()):
+    start_timestamp = datetime.datetime.now()
     input_filename = f'{filename}_l1_norm_30_{datetime.datetime.now()}{Path(image.filename).suffix}'
-    input_file = os.path.expanduser(f'~/Projects/Thesis/input_files/{input_filename}')
-    with open(input_file, "wb+") as file_object:
-        file_object.write(image.file.read())
-        file_object.close()
+    minio_host = os.popen("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio-server").read()
+    ip_host = os.popen("ip addr | awk '/inet/ && /docker0/ {print $2}'").read().split('/')[0]
+
+    # Upload FIle to Minio bucket
+    minio_client.put_object(
+        bucket_name=f'{bucket_name}',
+        object_name=f'input_files/{input_filename}',
+        data=image.file,
+        length=-1,  # Unknown size, read till EOF,
+        part_size=5 << 20 # 5MB chunks
+    )
+
+    # enqueue to redis
+    redis_client.lpush('srgan', json.dumps(dict(
+        filename=input_filename,
+        image_name='srgan-ai-module:l1_norm-30',
+        ai_type='srgan'
+    )))
+
+    # nanti disebelah sini akan dijalankan sebuah metode untuk menentukan proses yang datang ini akan
+    # dimasukkan pada worker mana ?
+    # 
+
+    # dequeue from redis
+    item = json.loads(redis_client.rpop('srgan').decode('utf-8'))
+    print(item)
+
     cmd = ["docker", "run", "--rm", "--runtime", 'nvidia',
            "-v", os.path.expanduser("~/Projects/Thesis/input_files:/app/input_files"),
            "-v", os.path.expanduser("~/Projects/Thesis/output_files:/app/output_files"),
-           "-e", f'FILENAME={input_filename}', "taufiqodhi/srgan-ai-module:l1_norm-30"]
+           "-e", f'FILENAME={input_filename}', "-e", f"MINIO_HOST={minio_host}",
+           "-e", f'IP_HOST={ip_host}', "-e", f"START_TIMESTAMP={start_timestamp}",
+           "taufiqodhi/srgan-ai-module:l1_norm-30"]
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     return dict(result=completed_process.stdout, error=completed_process.stderr)
 
 async def l1_norm_50(image: UploadFile = File(), filename: str = Form()):
+    start_timestamp = datetime.datetime.now()
     input_filename = f'{filename}_l1_norm_50_{datetime.datetime.now()}{Path(image.filename).suffix}'
-    input_file = os.path.expanduser(f'~/Projects/Thesis/input_files/{input_filename}')
-    with open(input_file, "wb+") as file_object:
-        file_object.write(image.file.read())
-        file_object.close()
+    minio_host = os.popen("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio-server").read()
+    ip_host = os.popen("ip addr | awk '/inet/ && /docker0/ {print $2}'").read().split('/')[0]
+
+    # Upload FIle to Minio bucket
+    minio_client.put_object(
+        bucket_name=f'{bucket_name}',
+        object_name=f'input_files/{input_filename}',
+        data=image.file,
+        length=-1,  # Unknown size, read till EOF,
+        part_size=5 << 20 # 5MB chunks
+    )
+
+    # enqueue to redis
+    redis_client.lpush('srgan', json.dumps(dict(
+        filename=input_filename,
+        image_name='srgan-ai-module:l1_norm-50',
+        ai_type='srgan'
+    )))
+
+    # nanti disebelah sini akan dijalankan sebuah metode untuk menentukan proses yang datang ini akan
+    # dimasukkan pada worker mana ?
+    # 
+
+    # dequeue from redis
+    item = json.loads(redis_client.rpop('srgan').decode('utf-8'))
+    print(item)
+
     cmd = ["docker", "run", "--rm", "--runtime", 'nvidia',
            "-v", os.path.expanduser("~/Projects/Thesis/input_files:/app/input_files"),
            "-v", os.path.expanduser("~/Projects/Thesis/output_files:/app/output_files"),
-           "-e", f'FILENAME={input_filename}', "taufiqodhi/srgan-ai-module:l1_norm-50"]
+           "-e", f'FILENAME={input_filename}', "-e", f"MINIO_HOST={minio_host}",
+           "-e", f'IP_HOST={ip_host}', "-e", f"START_TIMESTAMP={start_timestamp}",
+           "taufiqodhi/srgan-ai-module:l1_norm-50"]
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     return dict(result=completed_process.stdout, error=completed_process.stderr)
 
 async def l1_norm_70(image: UploadFile = File(), filename: str = Form()):
+    start_timestamp = datetime.datetime.now()
     input_filename = f'{filename}_l1_norm_70_{datetime.datetime.now()}{Path(image.filename).suffix}'
-    input_file = os.path.expanduser(f'~/Projects/Thesis/input_files/{input_filename}')
-    with open(input_file, "wb+") as file_object:
-        file_object.write(image.file.read())
-        file_object.close()
+    minio_host = os.popen("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio-server").read()
+    ip_host = os.popen("ip addr | awk '/inet/ && /docker0/ {print $2}'").read().split('/')[0]
+
+    # Upload FIle to Minio bucket
+    minio_client.put_object(
+        bucket_name=f'{bucket_name}',
+        object_name=f'input_files/{input_filename}',
+        data=image.file,
+        length=-1,  # Unknown size, read till EOF,
+        part_size=5 << 20 # 5MB chunks
+    )
+
+    # enqueue to redis
+    redis_client.lpush('srgan', json.dumps(dict(
+        filename=input_filename,
+        image_name='srgan-ai-module:l1_norm-70',
+        ai_type='srgan'
+    )))
+
+    # nanti disebelah sini akan dijalankan sebuah metode untuk menentukan proses yang datang ini akan
+    # dimasukkan pada worker mana ?
+    # 
+
+    # dequeue from redis
+    item = json.loads(redis_client.rpop('srgan').decode('utf-8'))
+    print(item)
+
     cmd = ["docker", "run", "--rm", "--runtime", 'nvidia',
            "-v", os.path.expanduser("~/Projects/Thesis/input_files:/app/input_files"),
            "-v", os.path.expanduser("~/Projects/Thesis/output_files:/app/output_files"),
-           "-e", f'FILENAME={input_filename}', "taufiqodhi/srgan-ai-module:l1_norm-70"]
+           "-e", f'FILENAME={input_filename}', "-e", f"MINIO_HOST={minio_host}",
+           "-e", f'IP_HOST={ip_host}', "-e", f"START_TIMESTAMP={start_timestamp}",
+           "taufiqodhi/srgan-ai-module:l1_norm-70"]
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     return dict(result=completed_process.stdout, error=completed_process.stderr)
 
 async def l2_norm_30(image: UploadFile = File(), filename: str = Form()):
+    start_timestamp = datetime.datetime.now()
     input_filename = f'{filename}_l2_norm_30_{datetime.datetime.now()}{Path(image.filename).suffix}'
-    input_file = os.path.expanduser(f'~/Projects/Thesis/input_files/{input_filename}')
-    with open(input_file, "wb+") as file_object:
-        file_object.write(image.file.read())
-        file_object.close()
+    minio_host = os.popen("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio-server").read()
+    ip_host = os.popen("ip addr | awk '/inet/ && /docker0/ {print $2}'").read().split('/')[0]
+
+    # Upload FIle to Minio bucket
+    minio_client.put_object(
+        bucket_name=f'{bucket_name}',
+        object_name=f'input_files/{input_filename}',
+        data=image.file,
+        length=-1,  # Unknown size, read till EOF,
+        part_size=5 << 20 # 5MB chunks
+    )
+
+    # enqueue to redis
+    redis_client.lpush('srgan', json.dumps(dict(
+        filename=input_filename,
+        image_name='srgan-ai-module:l2_norm-30',
+        ai_type='srgan'
+    )))
+
+    # nanti disebelah sini akan dijalankan sebuah metode untuk menentukan proses yang datang ini akan
+    # dimasukkan pada worker mana ?
+    # 
+
+    # dequeue from redis
+    item = json.loads(redis_client.rpop('srgan').decode('utf-8'))
+    print(item)
+
     cmd = ["docker", "run", "--rm", "--runtime", 'nvidia',
            "-v", os.path.expanduser("~/Projects/Thesis/input_files:/app/input_files"),
            "-v", os.path.expanduser("~/Projects/Thesis/output_files:/app/output_files"),
-           "-e", f'FILENAME={input_filename}', "taufiqodhi/srgan-ai-module:l2_norm-30"]
+           "-e", f'FILENAME={input_filename}', "-e", f"MINIO_HOST={minio_host}",
+           "-e", f'IP_HOST={ip_host}', "-e", f"START_TIMESTAMP={start_timestamp}",
+           "taufiqodhi/srgan-ai-module:l2_norm-30"]
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     return dict(result=completed_process.stdout, error=completed_process.stderr)
 
 async def l2_norm_50(image: UploadFile = File(), filename: str = Form()):
+    start_timestamp = datetime.datetime.now()
     input_filename = f'{filename}_l2_norm_50_{datetime.datetime.now()}{Path(image.filename).suffix}'
-    input_file = os.path.expanduser(f'~/Projects/Thesis/input_files/{input_filename}')
-    with open(input_file, "wb+") as file_object:
-        file_object.write(image.file.read())
-        file_object.close()
+    minio_host = os.popen("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio-server").read()
+    ip_host = os.popen("ip addr | awk '/inet/ && /docker0/ {print $2}'").read().split('/')[0]
+
+    # Upload FIle to Minio bucket
+    minio_client.put_object(
+        bucket_name=f'{bucket_name}',
+        object_name=f'input_files/{input_filename}',
+        data=image.file,
+        length=-1,  # Unknown size, read till EOF,
+        part_size=5 << 20 # 5MB chunks
+    )
+
+    # enqueue to redis
+    redis_client.lpush('srgan', json.dumps(dict(
+        filename=input_filename,
+        image_name='srgan-ai-module:l2_norm-50',
+        ai_type='srgan'
+    )))
+
+    # nanti disebelah sini akan dijalankan sebuah metode untuk menentukan proses yang datang ini akan
+    # dimasukkan pada worker mana ?
+    # 
+
+    # dequeue from redis
+    item = json.loads(redis_client.rpop('srgan').decode('utf-8'))
+    print(item)
+
     cmd = ["docker", "run", "--rm", "--runtime", 'nvidia',
            "-v", os.path.expanduser("~/Projects/Thesis/input_files:/app/input_files"),
            "-v", os.path.expanduser("~/Projects/Thesis/output_files:/app/output_files"),
-           "-e", f'FILENAME={input_filename}', "taufiqodhi/srgan-ai-module:l2_norm-50"]
+           "-e", f'FILENAME={input_filename}', "-e", f"MINIO_HOST={minio_host}",
+           "-e", f'IP_HOST={ip_host}', "-e", f"START_TIMESTAMP={start_timestamp}",
+           "taufiqodhi/srgan-ai-module:l2_norm-50"]
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     return dict(result=completed_process.stdout, error=completed_process.stderr)
 
 async def l2_norm_70(image: UploadFile = File(), filename: str = Form()):
+    start_timestamp = datetime.datetime.now()
     input_filename = f'{filename}_l2_norm_70_{datetime.datetime.now()}{Path(image.filename).suffix}'
-    input_file = os.path.expanduser(f'~/Projects/Thesis/input_files/{input_filename}')
-    with open(input_file, "wb+") as file_object:
-        file_object.write(image.file.read())
-        file_object.close()
+    minio_host = os.popen("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' minio-server").read()
+    ip_host = os.popen("ip addr | awk '/inet/ && /docker0/ {print $2}'").read().split('/')[0]
+
+    # Upload FIle to Minio bucket
+    minio_client.put_object(
+        bucket_name=f'{bucket_name}',
+        object_name=f'input_files/{input_filename}',
+        data=image.file,
+        length=-1,  # Unknown size, read till EOF,
+        part_size=5 << 20 # 5MB chunks
+    )
+
+    # enqueue to redis
+    redis_client.lpush('srgan', json.dumps(dict(
+        filename=input_filename,
+        image_name='srgan-ai-module:l2_norm-70',
+        ai_type='srgan'
+    )))
+
+    # nanti disebelah sini akan dijalankan sebuah metode untuk menentukan proses yang datang ini akan
+    # dimasukkan pada worker mana ?
+    # 
+
+    # dequeue from redis
+    item = json.loads(redis_client.rpop('srgan').decode('utf-8'))
+    print(item)
+
     cmd = ["docker", "run", "--rm", "--runtime", 'nvidia',
            "-v", os.path.expanduser("~/Projects/Thesis/input_files:/app/input_files"),
            "-v", os.path.expanduser("~/Projects/Thesis/output_files:/app/output_files"),
-           "-e", f'FILENAME={input_filename}', "taufiqodhi/srgan-ai-module:l2_norm-70"]
+           "-e", f'FILENAME={input_filename}',
+           "-e", f'FILENAME={input_filename}', "-e", f"MINIO_HOST={minio_host}",
+           "-e", f'IP_HOST={ip_host}', "-e", f"START_TIMESTAMP={start_timestamp}",
+           "taufiqodhi/srgan-ai-module:l2_norm-70"]
     completed_process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     
     return dict(result=completed_process.stdout, error=completed_process.stderr)
