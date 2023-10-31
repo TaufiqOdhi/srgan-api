04985a822ffe54466b7d51cd7869ee0e3e1ecf3b
Taufiq Odhi <taufiq.odhi@gmail.com>
2023-10-31 10:06:16 +0700

    update enpoint router

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -13,72 +13,24 @@ main_router.add_api_route(
 )
 
 main_router.add_api_route(
-    '/random_unstructured/30',
-    random_unstructured_30,
+    '/random_unstructured',
+    random_unstructured,
     methods=['post'],
     tags=['srgan'],
     # response_class=FileResponse
 )
 
 main_router.add_api_route(
-    '/random_unstructured/50',
-    random_unstructured_50,
+    '/l1_norm',
+    l1_norm,
     methods=['post'],
     tags=['srgan'],
     # response_class=FileResponse
 )
 
 main_router.add_api_route(
-    '/random_unstructured/70',
-    random_unstructured_70,
-    methods=['post'],
-    tags=['srgan'],
-    # response_class=FileResponse
-)
-
-main_router.add_api_route(
-    '/l1_norm_30',
-    l1_norm_30,
-    methods=['post'],
-    tags=['srgan'],
-    # response_class=FileResponse
-)
-
-main_router.add_api_route(
-    '/l1_norm_50',
-    l1_norm_50,
-    methods=['post'],
-    tags=['srgan'],
-    # response_class=FileResponse
-)
-
-main_router.add_api_route(
-    '/l1_norm_70',
-    l2_norm_70,
-    methods=['post'],
-    tags=['srgan'],
-    # response_class=FileResponse
-)
-
-main_router.add_api_route(
-    '/l2_norm_30',
-    l2_norm_30,
-    methods=['post'],
-    tags=['srgan'],
-    # response_class=FileResponse
-)
-
-main_router.add_api_route(
-    '/l2_norm_50',
-    l2_norm_50,
-    methods=['post'],
-    tags=['srgan'],
-    # response_class=FileResponse
-)
-
-main_router.add_api_route(
-    '/l2_norm_70',
-    l2_norm_70,
+    '/l2_norm',
+    l2_norm,
     methods=['post'],
     tags=['srgan'],
     # response_class=FileResponse
